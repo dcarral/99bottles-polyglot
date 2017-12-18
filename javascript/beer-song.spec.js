5951bc4e32810dfe1836cd88b0f8c1dd1a1979ea
Daniel Carral <dan@dcarral.org>
2017-12-18 12:49:58 +0100

    Mark full lyrics test as pending in JS

--- javascript/beer-song.spec.js
+++ javascript/beer-song.spec.js
@@ -45,7 +45,7 @@ describe('BeerSong', function () {
     expect(beerSong.verses(2, 0)).toEqual(expected);
   });
 
-  it('handles full lyrics', function () {
+  xit('handles full lyrics', function () {
     var expected = `99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
