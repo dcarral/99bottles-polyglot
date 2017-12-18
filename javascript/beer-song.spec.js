4c48cdfc6ceae045ccbc0d5c5f5d9008d2e91f47
Daniel Carral <dan@dcarral.org>
2017-12-18 12:24:49 +0100

    Unify test suites, modify JavaScript's one

--- javascript/beer-song.spec.js
+++ javascript/beer-song.spec.js
@@ -3,33 +3,38 @@ var BeerSong = require('./beer-song');
 describe('BeerSong', function () {
   var song = new BeerSong();
 
-  it('prints an arbitrary verse', function () {
-    var expected = '8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n';
-    expect(song.verse(8)).toEqual(expected);
+  it('handles first generic verse', function () {
+    var expected = '99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n';
+    expect(song.verse(99)).toEqual(expected);
   });
 
-  xit('handles 2 bottles', function () {
+  xit('handles last generic verse', function () {
+    var expected = '3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n';
+    expect(song.verse(3)).toEqual(expected);
+  });
+
+  xit('handles third to last verse', function () {
     var expected = '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n';
     expect(song.verse(2)).toEqual(expected);
   });
 
-  xit('handles 1 bottle', function () {
+  xit('handles penultimate verse', function () {
     var expected = '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
     expect(song.verse(1)).toEqual(expected);
   });
 
-  xit('handles 0 bottles', function () {
+  xit('handles last verse', function () {
     var expected = 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
     expect(song.verse(0)).toEqual(expected);
   });
 
-  xit('sings several verses', function () {
-    var expected = '8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n';
-    expect(song.sing(8, 6)).toEqual(expected);
+  xit('handles first three verses', function () {
+    var expected = '99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n\n98 bottles of beer on the wall, 98 bottles of beer.\nTake one down and pass it around, 97 bottles of beer on the wall.\n\n97 bottles of beer on the wall, 97 bottles of beer.\nTake one down and pass it around, 96 bottles of beer on the wall.\n';
+    expect(song.verses(99, 97)).toEqual(expected);
   });
 
-  xit('sings the rest of the verses', function () {
-    var expected = '3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
-    expect(song.sing(3)).toEqual(expected);
+  xit('handles last three verses', function () {
+    var expected = '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
+    expect(song.verses(2, 0)).toEqual(expected);
   });
 });
