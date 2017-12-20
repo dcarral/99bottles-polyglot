19a9a4f37db2a35c66b637ba41c71d01085e3373
Daniel Teske <danielteske@dante.local>
2017-12-20 16:33:03 +0100

    missing newline at the end

--- kotlin/src/test/kotlin/BeerSongTest.kt
+++ kotlin/src/test/kotlin/BeerSongTest.kt
@@ -355,6 +355,7 @@ Take it down and pass it around, no more bottles of beer on the wall.
 
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
+
 """
         assertEquals(expected, BeerSong.lyrics())
     }
