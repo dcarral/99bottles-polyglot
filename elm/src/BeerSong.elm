d6fed82915f75e5b628ae22e0a3c205e82ac05f0
rodrigo-morais <morais.rm@gmail.com>
2017-12-21 15:38:53 +0100

    Adding container to Elm

---
+++ elm/src/BeerSong.elm
@@ -0,0 +1,13 @@
+module BeerSong exposing (verse, verses, sing)
+
+verse : Int -> String
+verse number =
+  ""
+
+verses : Int -> Int -> String
+verses begin end =
+  ""
+
+sing : String
+sing =
+  ""
